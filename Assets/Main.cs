5d70dccf3568f41633a6a1f027d7315f82e7be44
simoom22 <swatson4@kent.edu>
2013-10-07 14:45:16 -0400

    first commit of simple physics dragging game

---
+++ Assets/Main.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class Main : MonoBehaviour {
+	
+	public static Main instance;
+	public GameObject playerPrefab;
+	public GameObject targetPrefab;
+	
+	
+	void Awake () {
+		if (instance != null) {
+			throw new Exception("Attempted to create more than one instance of Main");
+		}
+		instance = this;
+	}
+	
+	// Use this for initialization
+	void Start () {
+		if (playerPrefab != null) {
+			GameObject.Instantiate(playerPrefab);
+		}
+		if (targetPrefab != null) {
+			GameObject.Instantiate(targetPrefab);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public static Main Get() {
+		return instance;
+	}
+	
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+	private Transform trans;
+	private Rigidbody rb;
+	public int speed = 1;
+	
+	//control stuff- platform detection
+	
+	// Use this for initialization
+	void Start () {
+		trans = transform;
+		rb = trans.rigidbody;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void FixedUpdate () {
+		//directed movement - mouse only
+		var cam = Camera.main;
+		if (Input.GetMouseButton(1)) {
+			Ray inputRay = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(inputRay.origin, inputRay.direction, out hit)) {
+				var dir = (hit.point - trans.position).normalized;
+				//dir.y = trans.position.y;
+				rb.AddForce(dir, ForceMode.Impulse);
+				
+//				var point = hit.point;
+//				point.y = 1;
+//				var step = speed * Time.deltaTime;
+//				trans.position = Vector3.Lerp(trans.position, point, step);
+			}
+		}
+	}
+}
